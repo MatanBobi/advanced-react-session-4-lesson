0536aeba716d7d38b070271c5e913c99b5537596
Matan Borenkraout <matanbobi@gmail.com>
2023-01-07 12:21:43 +0200

    add a higher boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
+import { Suspense } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import { Home } from "./Home";
 import { PokemonPage } from "./PokemonPage";
+import Spinner from "./Spinner/Spinner";
 
 export function App() {
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/pokemon/:id" component={PokemonPage} />
-          <Route path="/" component={Home} />
-        </Switch>
-      </Router>
+      <Suspense fallback={<Spinner />}>
+        <Router>
+          <Switch>
+            <Route path="/pokemon/:id" component={PokemonPage} />
+            <Route path="/" component={Home} />
+          </Switch>
+        </Router>
+      </Suspense>
     </div>
   );
 }
